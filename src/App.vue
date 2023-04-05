2ca9452d41501dafb83d25242eb9ba37e0256db0
Axios <diman0719192323@gmail.com>
2023-04-05 16:50:30 +0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,35 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <h1>{{ name }}</h1>
+  <h3>{{ age }}</h3>
+  <button @click="changeName(32)">Change Name!</button>
+  <button @click="changeAge(true)">Change Age!</button>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import {defineComponent} from "vue";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default defineComponent({
+  name: 'App',
+  components: {},
+  data() {
+    return {
+      name: 'Link',
+      age: 23 as number | string
     }
-  }
-}
+  },
+  methods: {
+    changeName(newName: string) {
+      this.name = newName
+    },
+    changeAge(newAge: number | string) {
+      this.age = newAge
+    }
+  },
+})
+
+</script>
+
+
+<style lang="scss">
+@import "./assets/styles/style.scss";
 </style>
